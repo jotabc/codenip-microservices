995f4b36dfdd7dffb6048043e4a1613b5c8eed9c
jonnathan <jonnabcl56@gmail.com>
2023-12-30 14:31:54 -0500

    register service config rabbitmq message

--- register/src/Controller/RegisterAction.php
+++ register/src/Controller/RegisterAction.php
@@ -3,18 +3,27 @@
 namespace App\Controller;
 
 use App\Http\DTO\RegisterRequest;
+use App\Services\RegisterActionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterAction extends AbstractController
 {
+    private RegisterActionService $registerActionService;
+
+    public function __construct(RegisterActionService $registerActionService)
+    {
+        $this->registerActionService = $registerActionService;
+    }
+
     /**
      * @Route("/api/register", name="api_register", methods={"POST"})
      */
     public function __invoke(RegisterRequest $registerRequest): JsonResponse
     {
-        return  new JsonResponse();
+        $this->registerActionService->__invoke($registerRequest->getName(), $registerRequest->getEmail());
+        return new JsonResponse();
     }
 
 }
---
+++ register/src/Messenger/RoutingKey.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Messenger;
+
+abstract class RoutingKey
+{
+    public const REGISTER_APPLICATION_QUEUE = 'register_application_queue';
+}
